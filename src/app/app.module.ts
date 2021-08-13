4ef8da1a66903cb6139ba07b55fb0123761228b5
Nikos Anifantis <nikosanif@gmail.com>
2021-08-13 19:04:14 +0300

    chore: add more strict rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/app/app.component.spec.ts
+++ src/app/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
