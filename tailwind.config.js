501bcb317ad904e7a5679d03b31e5df1d89054f3
Nikos Anifantis <nikosanif@gmail.com>
2021-10-13 11:49:35 +0300

    build(workspace): install and setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: 'aa-',
+  important: true,
+  mode: process.env.TAILWIND_MODE ? 'jit' : '',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.{html,ts}'],
+  },
+  darkMode: false, // false or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
