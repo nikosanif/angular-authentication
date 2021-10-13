2c7e3084d2533a929815febb4bcf63f914b4acb6
Nikos Anifantis <nikosanif@gmail.com>
2021-10-13 16:05:57 +0300

    feat: add settings at environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  apiUrl: '<Your auth server API URL here>',
+  settings: {
+    auth: {
+      clientId: '<Your auth client id here>',
+      secretId: '<Your auth secret id here>',
+      // keys to store tokens at local storage
+      accessTokenKey: 'DoPS3ZrQjM',
+      refreshTokenKey: 'nmlP8PW2nb',
+    },
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  apiUrl: '<Your auth server API URL here>',
+  settings: {
+    auth: {
+      clientId: '<Your auth client id here>',
+      secretId: '<Your auth secret id here>',
+      // keys to store tokens at local storage
+      accessTokenKey: 'DoPS3ZrQjM',
+      refreshTokenKey: 'nmlP8PW2nb',
+    },
+  },
 };
 
 /*
