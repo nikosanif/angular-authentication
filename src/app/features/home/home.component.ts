c5578b0fdc54d2808d81ffa9e57d7a66641b62f3
Nikos Anifantis <nikosanif@gmail.com>
2021-10-13 11:53:55 +0300

    refactor(app): change app prefix

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'aa-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
