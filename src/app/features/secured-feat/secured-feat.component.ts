d76f4f0ffd4dfb9ca9afd546a8b8f8b20c21c85a
Nikos Anifantis <nikosanif@gmail.com>
2021-10-19 16:36:56 +0300

    feat: add content at secured-feat component

--- src/app/features/secured-feat/secured-feat.component.ts
+++ src/app/features/secured-feat/secured-feat.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+import { AuthFacade } from '../../auth/store/auth.facade';
+import { GreetingUtil } from '../../shared/util';
 @Component({
   selector: 'aa-secured-feat',
   templateUrl: './secured-feat.component.html',
 })
-export class SecuredFeatComponent {}
+export class SecuredFeatComponent {
+  greeting = GreetingUtil.greet();
+  user$ = this.authFacade.user$;
+
+  constructor(private authFacade: AuthFacade) {}
+}
--- src/app/features/secured-feat/secured-feat.module.ts
+++ src/app/features/secured-feat/secured-feat.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveComponentModule } from '@ngrx/component';
 
 import { SecuredFeatComponent } from './secured-feat.component';
 
 const routes: Routes = [{ path: '', component: SecuredFeatComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, ReactiveComponentModule, RouterModule.forChild(routes)],
   declarations: [SecuredFeatComponent],
 })
 export class SecuredFeatModule {}
