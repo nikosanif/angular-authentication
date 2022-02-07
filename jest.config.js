f33bce27f9a3ed215b3e85e0c35c20be8040fa86
Nikos Anifantis <nikosanif@gmail.com>
2022-02-07 12:15:39 +0200

    build: update jest and its config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testRunner: 'jest-jasmine2',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
