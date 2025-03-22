0a8281da6076d5800043f38a4d6423d1c402f266
“akib1997” <akibh805@gmail.comgit config --global user.name akib1997>
2025-03-22 16:43:57 +0600

    refactor: replace GreetingUtil with a pipe for better testability

--- src/app/features/secured-feat/secured-feat.component.ts
+++ src/app/features/secured-feat/secured-feat.component.ts
@@ -1,24 +1,24 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { combineLatest, of } from 'rxjs';
 
 import { AUTH_FACADE } from '../../auth';
 import { USERS } from '../../core/fake-api';
-import { GreetingUtil } from '../../shared/util';
+import { GreetingPipe } from '../../shared/pipes';
 @Component({
   selector: 'aa-secured-feat',
-  imports: [AsyncPipe, MatCardModule, MatTableModule],
+  imports: [AsyncPipe, MatCardModule, MatTableModule, GreetingPipe],
   templateUrl: './secured-feat.component.html',
 })
 export class SecuredFeatComponent {
   private readonly authFacade = inject(AUTH_FACADE);
 
   readonly displayedColumns: string[] = ['id', 'name', 'username', 'password'];
+  readonly currentTime = signal(new Date());
 
   readonly vm$ = combineLatest({
-    greeting: of(GreetingUtil.greet()),
     authUser: this.authFacade.authUser$,
     users: of(USERS),
   });
